516e89dfd4b3951d60e60d3e0c7a6e3e10786379
Matheo <garerimath73660@gmail.com>
2023-12-03 15:23:47 +0100

    add Invitations

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -3,8 +3,9 @@
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
 
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { ChevronDown, Settings, UserPlus } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
     server: ServerWithMembersWithProfiles;
@@ -12,8 +13,10 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+    const { onOpen } = useModal();
+
     const isAdmin = role === MemberRole.ADMIN;
-    const isModerator = isAdmin ||role === MemberRole.MODERATOR;
+    const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
     return (
         <DropdownMenu>
@@ -25,7 +28,9 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-100 space-y-[2px]">
                 {isModerator && (
-                    <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                    onClick={() => onOpen("invite", { server: server })}
+                    className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
                         Invite People
                         <UserPlus className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
@@ -34,6 +39,33 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                     <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
                         Server Settings
                         <Settings className="h-4 w-4 ml-auto" />
+                    </DropdownMenuItem>
+                )}
+                {isAdmin && (
+                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                        Manage Members
+                        <Users className="h-4 w-4 ml-auto" />
+                    </DropdownMenuItem>
+                )}
+                {isModerator && (
+                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                        Create Channel
+                        <PlusCircle className="h-4 w-4 ml-auto" />
+                    </DropdownMenuItem>
+                )}
+                {isModerator && (
+                    <DropdownMenuSeparator />
+                )}
+                {isAdmin && (
+                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                        Delete Server
+                        <Trash className="h-4 w-4 ml-auto" />
+                    </DropdownMenuItem>
+                )}
+                {!isAdmin && (
+                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                        Leave Server
+                        <LogOut className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
                 )}
             </DropdownMenuContent>
