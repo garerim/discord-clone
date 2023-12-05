8a0b52357158340c438e06ed77a555dff542cc5d
Matheo <garerimath73660@gmail.com>
2023-12-05 21:54:04 +0100

    add leave/delete server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -64,13 +64,17 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                     <DropdownMenuSeparator />
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                    onClick={() => onOpen("deleteServer", { server: server })}
+                    className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Delete Server
                         <Trash className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
                 )}
                 {!isAdmin && (
-                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem 
+                    onClick={() => onOpen("leaveServer", { server: server })}
+                    className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
