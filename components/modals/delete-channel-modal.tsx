86c5872728f31eea5ac978700d551ce64d5253a5
Matheo <garerimath73660@gmail.com>
2023-12-09 00:43:17 +0100

    Add edit/delete channel

--- components/modals/delete-channel-modal.tsx
+++ components/modals/delete-channel-modal.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 
+import qs from 'query-string';
 import { Button } from "@/components/ui/button";
 import {
     Dialog,
@@ -13,26 +14,33 @@ import {
 } from "@/components/ui/dialog";
 import { useModal } from "@/hooks/use-modal-store";
 import axios from "axios";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
-export const DeleteServerModal = () => {
+export const DeleteChannelModal = () => {
     const { isOpen, onClose, type, data } = useModal();
     const router = useRouter();
 
-    const isModalOpen = isOpen && type === "deleteServer";
-    const { server } = data;
+    const isModalOpen = isOpen && type === "deleteChannel";
+    const { server, channel } = data;
 
     const [isLoading, setIsLoading] = useState(false);
 
     const onClick = async () => {
         try {
             setIsLoading(true)
+            const url = qs.stringifyUrl({
+                url: `/api/channels/${channel?.id}`,
+                query: {
+                    serverId: server?.id,
+                }
+            })
 
-            await axios.delete(`/api/servers/${server?.id}`);
+            await axios.delete(url);
 
             onClose();
             router.refresh();
-            router.push('/')
+            router.push(`/servers/${server?.id}`);
+            router.refresh();
         } catch (error) {
             console.log(error);
         } finally {
@@ -45,11 +53,11 @@ export const DeleteServerModal = () => {
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Delete Server
+                        Delete Channel
                     </DialogTitle>
                     <DialogDescription className="text-center text-zinc-500">
                         Are you sure you want to do this ? <br />
-                        <span className="text-indigo-500 font-semibold">{server?.name}</span> will be permanently deleted.
+                        <span className="text-indigo-500 font-semibold">#{channel?.name}</span> will be permanently deleted.
                     </DialogDescription>
                 </DialogHeader>
                 <DialogFooter className="bg-grey-100 px-6 py-4">
--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import { Edit, Hash, Lock, Mic, Trash, Video } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
-import { ActionTooltip } from "../action-tootip";
+
+import { cn } from "@/lib/utils";
+import { Channel, ChannelType, MemberRole, Server } from "@prisma/client"; 
+import { ActionTooltip } from "@/components/action-tootip";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps {
     channel: Channel;
@@ -24,6 +26,7 @@ export const ServerChannel = ({
     role
 }: ServerChannelProps) => {
 
+    const {onOpen} = useModal();
     const params = useParams();
     const router = useRouter();
 
@@ -39,10 +42,14 @@ export const ServerChannel = ({
             {channel.name !== "general" && role !== MemberRole.GUEST && (
                 <div className="ml-auto flex items-center gap-x-2">
                     <ActionTooltip label="Edit">
-                        <Edit className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+                        <Edit 
+                        onClick={() => onOpen("editChannel", { server, channel })}
+                        className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
                     </ActionTooltip>
                     <ActionTooltip label="Delete">
-                        <Trash className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+                        <Trash 
+                        onClick={() => onOpen("deleteChannel", { server, channel })}
+                        className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
                     </ActionTooltip>
                 </div>
             )}
