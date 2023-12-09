5deb774b8bf7957e84ca2ec98197e9732995da5f
Matheo <garerimath73660@gmail.com>
2023-12-09 12:14:37 +0100

    add chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const ChannelIdPage = () => {
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string
+    channelId: string
+  }
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    }
+  })
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id
+    }
+  })
+
+  if (!channel || !member) {
+    redirect('/');
+  }
+
   return (
-    <div>Channel Id Page</div>
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
+    </div>
   )
 }
 
